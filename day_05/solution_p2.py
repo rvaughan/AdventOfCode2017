b07915b4770af373a7abf84519c73e9d08bbb29f
Richard Vaughan <richard@purplemonkeys.co.uk>
2017-12-05 07:00:53 +0000

    Solution for day 5, part 2.

---
+++ day_05/solution_p2.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import sys
+
+
+def process_stack(stack_data):
+    ip = 0
+
+    instruction_count = 0
+    while ip >= 0 and ip < len(stack_data):
+        cur_inst = stack_data[ip]
+
+        if stack_data[ip] >= 3:
+            stack_data[ip] -= 1
+        else:
+            stack_data[ip] += 1
+
+        ip += cur_inst
+
+        instruction_count += 1
+
+    return instruction_count
+
+
+test_data = [0, 3, 0, 1, -3]
+expected_steps = 10
+num_steps = process_stack(test_data)
+if expected_steps != num_steps:
+    print "Test failed."
+    sys.exit(-1)
+print "Test passed."
+
+instruction_data = []
+with open("input.txt", "r") as f:
+    for instruction in f.readlines():
+        instruction_data.append(int(instruction))
+
+    print process_stack(instruction_data)
