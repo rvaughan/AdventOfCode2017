3633334ed4ac818afa50a8a0f93c62fa04555faf
Richard Vaughan <richard@purplemonkeys.co.uk>
2017-12-15 06:06:16 +0000

    Solution for day 15, part 1.

--- day_15/solution_p1.py
+++ day_15/solution_p1.py
@@ -2,3 +2,28 @@
 
 import sys
 
+GEN_A = 65
+GEN_A_FACTOR = 16807
+GEN_B = 8921
+GEN_B_FACTOR = 48271
+
+COUNT = 0
+for _ in xrange(5):
+    GEN_A = (GEN_A * GEN_A_FACTOR) % 2147483647
+    GEN_B = (GEN_B * GEN_B_FACTOR) % 2147483647
+    if GEN_A & 0xFFFF == GEN_B & 0xFFFF:
+        COUNT += 1
+if COUNT != 1:
+    print "Test failed."
+    sys.exit(-1)
+print "Tests pass"
+
+GEN_A = 618
+GEN_B = 814
+COUNT = 0
+for _ in xrange(40000000):
+    GEN_A = (GEN_A * GEN_A_FACTOR) % 2147483647
+    GEN_B = (GEN_B * GEN_B_FACTOR) % 2147483647
+    if GEN_A & 0xFFFF == GEN_B & 0xFFFF:
+        COUNT += 1
+print COUNT
