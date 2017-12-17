7089a7578338deaaab6a5b3c8fb5b8ca4afcfde2
Richard Vaughan <richard@purplemonkeys.co.uk>
2017-12-17 11:44:26 +0000

    Code cleanup for day 2, part 1.

--- day_02/solution_p1.py
+++ day_02/solution_p1.py
@@ -4,6 +4,9 @@ import sys
 
 
 def calc_row_checksum(row_data):
+    """
+    Method for calculating the checksum for a row.
+    """
     min_val = 0
     max_val = 0
 
@@ -24,22 +27,27 @@ def calc_row_checksum(row_data):
 
     return max_val - min_val
 
+# Tests for validating code solution
+
 with open("test_spreadsheet.txt", "r") as f:
-    checksum = 0
+    CHECKSUM = 0
 
     for row in f.readlines():
-        checksum += calc_row_checksum(row)
+        CHECKSUM += calc_row_checksum(row)
 
-    if checksum != 18:
-        print "Invalid test checksum, got {0} not, 18".format(checksum)
+    if CHECKSUM != 18:
+        print "Invalid test checksum, got {0} not, 18".format(CHECKSUM)
         sys.exit(-1)
+    print "Checksum test passed."
+
+print "All tests passed."
 
 # If we reach here, then our test has passed and we will proceed.
 
 with open("input.txt", "r") as f:
-    checksum = 0
+    CHECKSUM = 0
 
     for row in f.readlines():
-        checksum += calc_row_checksum(row)
+        CHECKSUM += calc_row_checksum(row)
 
-    print checksum
+    print CHECKSUM
