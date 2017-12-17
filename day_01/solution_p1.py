82395d49c6080d664d1975d8ca443334e232dc81
Richard Vaughan <richard@purplemonkeys.co.uk>
2017-12-17 11:12:57 +0000

    Code comment updates.

--- day_01/solution_p1.py
+++ day_01/solution_p1.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
+"""
+This code holds the solution for part 1 of day 1 of the Advent of Code for 2017.
+"""
 
 import sys
 
 
 def solve_captcha(captcha):
+    """
+    Method for trying to solve the captcha.
+    """
     solution = 0
 
     prev_digit = captcha[-1]
@@ -18,6 +24,9 @@ def solve_captcha(captcha):
 
 
 def run_test(test_captcha, expected_solution):
+    """
+    Helper method for running some unit tests whilst minimising repetative code.
+    """
     result = solve_captcha(test_captcha)
 
     if result != expected_solution:
@@ -26,6 +35,8 @@ def run_test(test_captcha, expected_solution):
 
     print "Test for captcha {0} passed.".format(test_captcha)
 
+# Run any tests that we've defined to help validate our code prior to
+# trying to solve the puzzle.
 
 run_test("1122", 3)
 run_test("1111", 4)
@@ -36,6 +47,6 @@ run_test("91212129", 9)
 # above is working correctly. Let's use the actual captcha now.
 
 with open("input.txt", "r") as f:
-    captcha = f.readline()
+    CAPTCHA = f.readline()
 
-    print "Captcha solution is {0}".format(solve_captcha(captcha))
+    print "Captcha solution is {0}".format(solve_captcha(CAPTCHA))
