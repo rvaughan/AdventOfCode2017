38ce31c7bf0f4538d264ed90bdcdde9cef210c36
Richard Vaughan <richard@purplemonkeys.co.uk>
2017-12-17 11:15:48 +0000

    Code comments for day 1, part 2.

--- day_01/solution_p2.py
+++ day_01/solution_p2.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python
+"""
+This code holds the solution for part 2 of day 1 of the Advent of Code for 2017.
+"""
 
 import sys
 
 
 def check_digits(digit_a, digit_b):
+    """
+    Check to see if the two digits are the same.
+    """
     return digit_a == digit_b
 
+
 def solve_captcha(captcha):
+    """
+    Method for solving the captcha.
+    """
     solution = 0
 
     list_size = len(captcha)
@@ -26,6 +36,9 @@ def solve_captcha(captcha):
 
 
 def run_test(test_captcha, expected_solution):
+    """
+    Helper method for running some unit tests whilst minimising repetative code.
+    """
     result = solve_captcha(test_captcha)
 
     if result != expected_solution:
@@ -34,6 +47,8 @@ def run_test(test_captcha, expected_solution):
 
     print "Test for captcha {0} passed.".format(test_captcha)
 
+# Run any tests that we've defined to help validate our code prior to
+# trying to solve the puzzle.
 
 run_test("1212", 6)
 run_test("1221", 0)
@@ -45,6 +60,6 @@ run_test("12131415", 4)
 # above is working correctly. Let's use the actual captcha now.
 
 with open("input.txt", "r") as f:
-    captcha = f.readline()
+    CAPTCHA = f.readline()
 
-    print "Captcha solution is {0}".format(solve_captcha(captcha))
+    print "Captcha solution is {0}".format(solve_captcha(CAPTCHA))
