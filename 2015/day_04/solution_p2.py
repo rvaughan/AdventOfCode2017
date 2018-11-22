f804d6c1562c39d491c167f31f044a738f76e57c
Richard Vaughan <richard@purplemonkeys.co.uk>
2018-11-22 16:51:49 +0000

    Added solution code for 2015, day 4, part 2.

---
+++ 2015/day_04/solution_p2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+"""
+This code holds the solution for part 1 of day 4 of the Advent of Code for 2015.
+"""
+import hashlib
+import sys
+
+def calc_hash(input_data):
+    for x in xrange(99999999999):
+        hash_md5 = hashlib.md5()
+
+        hash_input = "{}{}".format(input_data, x)
+
+        hash_md5.update(hash_input)
+        
+        if hash_md5.hexdigest().startswith("000000"):
+            return x
+
+    return 0
+
+with open("input.txt", "r") as f:
+    line = f.readline()
+    
+    result = calc_hash(line)
+
+    print "Solution: {}".format(result)
