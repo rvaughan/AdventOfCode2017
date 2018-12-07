dc94dc0cc5dff074c4b954e785d39ad8cc483f91
Richard Vaughan <richard@purplemonkeys.co.uk>
2018-12-07 14:50:36 +0000

    Small tweak.

--- 2015/day_16/solution_p2.py
+++ 2015/day_16/solution_p2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-This code holds the solution for part 1 of day 16 of the Advent of Code for 2015.
+This code holds the solution for part 2 of day 16 of the Advent of Code for 2015.
 """
 
 known_facts = """children: 3
