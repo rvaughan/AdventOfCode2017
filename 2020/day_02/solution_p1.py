b7fff1460f4a9c10aca440ddf6fc99a31e82a6d7
Richard Vaughan <richard@purplemonkeys.co.uk>
2020-12-02 06:46:39 +0000

    Add solution for 2020 day 2 part 1.

--- 2020/day_02/solution_p1.py
+++ 2020/day_02/solution_p1.py
@@ -3,12 +3,26 @@
 This code holds the solution for part 1 of day 2 of the Advent of Code for 2020.
 """
 
-import math
+from collections import defaultdict
 import sys
 
 
-def calculate_solution(items):
-    return 0
+def calculate_solution(passwords):
+    count = 0
+    for password in passwords:
+        parts = password.split()
+
+        min, max = parts[0].split('-')
+        character = parts[1].split(':')[0]
+
+        letters = defaultdict(int)
+        for x in parts[2]:
+            letters[x] += 1
+
+        if letters[character] >= int(min) and letters[character] <= int(max):
+            count += 1
+
+    return count
 
 
 def run_test(test_input, expected_solution):
@@ -18,10 +32,10 @@ def run_test(test_input, expected_solution):
     result = calculate_solution(test_input)
 
     if result != expected_solution:
-        print "Test for mass {0} FAILED. Got a result of {1}, not {2}".format(test_input, result, expected_solution)
+        print "Test for input {0} FAILED. Got a result of {1}, not {2}".format(test_input, result, expected_solution)
         sys.exit(-1)
 
-    print "Test for mass {0} passed.".format(test_input)
+    print "Test for input {0} passed.".format(test_input)
 
     return result
 
@@ -29,9 +43,13 @@ def run_test(test_input, expected_solution):
 # Run any tests that we've defined to help validate our code prior to
 # trying to solve the puzzle.
 
-test_list = []
+test_list = [
+    "1-3 a: abcde",
+    "1-3 b: cdefg",
+    "2-9 c: ccccccccc"
+]
 
-result = run_test(test_list, 1)
+result = run_test(test_list, 2)
 
 print ""
 print "-----------------"
@@ -43,7 +61,7 @@ print ""
 # above is working correctly. Let's use the actual captcha now.
 
 with open("input.txt", "r") as f:
-    input_data = [int(line) for line in f]
+    input_data = [line for line in f]
     answer = calculate_solution(input_data)
 
     print "Solution is {0}".format(answer)
