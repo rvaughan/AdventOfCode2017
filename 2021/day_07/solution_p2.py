dbd771575a881bc77ab540ae04532f2ad53b4053
Richard Vaughan <richard@purplemonkeys.co.uk>
2021-12-07 05:38:55 +0000

    Add solution for 2021 day 7 part 2.

---
+++ 2021/day_07/solution_p2.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+"""
+This code holds the solution for part 2 of day 7 of the Advent of Code for 2021.
+"""
+
+import sys
+from collections import defaultdict
+
+
+def calc_fuel_use(pos, input_data):
+    fuel = 0
+    for p in input_data:
+        fuel += sum(range(abs(p - pos) + 1))
+
+    return fuel
+
+
+def calculate_solution(input_data):
+    max_pos = max(input_data)
+
+    best_pos = 0
+    best_fuel = 9999999999
+    for pos in range(max_pos):
+        fuel = calc_fuel_use(pos, input_data)
+
+        if fuel < best_fuel:
+            best_fuel = fuel
+            best_pos = pos
+
+    return best_pos, best_fuel
+
+
+def run_test(test_input, expected_pos, expected_fuel):
+    """
+    Helper method for running some unit tests whilst minimising repetative code.
+    """
+    pos, fuel = calculate_solution([int(item) for item in test_input.split(',')])
+
+    if fuel != expected_fuel:
+        print(f'Test for {test_input} FAILED. Got a result of {fuel}, not {expected_fuel}')
+        sys.exit(-1)
+
+    print(f'Test for {test_input} passed.')
+
+    return fuel
+
+
+# Run any tests that we've defined to help validate our code prior to
+# trying to solve the puzzle.
+
+test_input = """16,1,2,0,4,2,7,1,2,14"""
+
+score = calc_fuel_use(5, [16,1,2,0,4,2,7,1,2,14])
+assert score == 168, score
+
+score = calc_fuel_use(2, [16,1,2,0,4,2,7,1,2,14])
+assert score == 206, score
+
+result = run_test(test_input, 5, 168)
+
+print('')
+print('-----------------')
+print('All Tests PASSED.')
+print('-----------------')
+print('')
+
+# Ok, so if we reach here, then we can be reasonably sure that the code
+# above is working correctly. Let's use the actual captcha now.
+
+with open('input.txt', 'r') as f:
+    line = f.readline()
+    input_data = [int(item) for item in line.split(',')]
+    pos, answer = calculate_solution(input_data)
+
+    print(f'Solution is {answer}')
