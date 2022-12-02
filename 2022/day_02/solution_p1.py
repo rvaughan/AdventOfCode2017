108b4964fd4f59a1e3e672550ddb4eed36ec9824
Richard Vaughan <richard@purplemonkeys.co.uk>
2022-12-02 06:39:58 +0000

    Add solution for 2022, day 2, part 1

--- 2022/day_02/solution_p1.py
+++ 2022/day_02/solution_p1.py
@@ -1,13 +1,59 @@
 #!/usr/bin/env python3
 """
-This code holds the solution for part 1 of day 2 of the Advent of Code for 2021.
+This code holds the solution for part 1 of day 2 of the Advent of Code for 2022.
 """
 
 import sys
 
+winning_moves = {
+    'Paper': 'Rock',
+    'Scissors': 'Paper',
+    'Rock': 'Scissors',
+}
+
+decoded_moves = {
+    'A': 'Rock',
+    'B': 'Paper',
+    'C': 'Scissors',
+    'X': 'Rock',
+    'Y': 'Paper',
+    'Z': 'Scissors',
+}
+
+win = 6
+draw = 3
+loss = 0
+
+move_scores = {
+    'Rock': 1,
+    'Paper': 2,
+    'Scissors': 3
+}
+
+
+def score_move(opp_move, your_move):
+    score = 0
+
+    score += move_scores[decoded_moves[your_move]]
+
+    # print(f'{decoded_moves[your_move]} {decoded_moves[opp_move]} {winning_moves[decoded_moves[your_move]]}')
+
+    if winning_moves[decoded_moves[your_move]] == decoded_moves[opp_move]:
+        return score + win
+
+    if decoded_moves[opp_move] == decoded_moves[your_move]:
+        return score + draw
+
+    return score+ loss
+
 
 def calculate_solution(items):
     result = 0
+
+    for item in items:
+        opp_move, your_move = item.split(" ")
+
+        result += score_move(opp_move, your_move)
 
     return result
 
@@ -19,7 +65,8 @@ def run_test(test_input, expected_solution):
     result = calculate_solution(test_input)
 
     if result != expected_solution:
-        print(f'Test for {test_input} FAILED. Got a result of {result}, not {expected_solution}')
+        print(
+            f'Test for {test_input} FAILED. Got a result of {result}, not {expected_solution}')
         sys.exit(-1)
 
     print(f'Test for {test_input} passed.')
@@ -30,9 +77,9 @@ def run_test(test_input, expected_solution):
 # Run any tests that we've defined to help validate our code prior to
 # trying to solve the puzzle.
 
-test_list = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+test_list = ["A Y", "B X", "C Z"]
 
-result = run_test(test_list, 7)
+result = run_test(test_list, 15)
 
 print('')
 print('-----------------')
@@ -44,7 +91,7 @@ print('')
 # above is working correctly. Let's use the actual captcha now.
 
 with open('input.txt', 'r') as f:
-    input_data = [int(line) for line in f]
+    input_data = [line.strip() for line in f]
     answer = calculate_solution(input_data)
 
     print(f'Solution is {answer}')
