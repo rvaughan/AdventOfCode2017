f3b70a165879c34c4bcf1a9841428be5c2b24c59
Richard Vaughan <richard@purplemonkeys.co.uk>
2022-12-03 08:44:13 +0000

    Add solution for 2022 day 3.

--- 2022/day_03/solution_p1.py
+++ 2022/day_03/solution_p1.py
@@ -6,20 +6,52 @@ This code holds the solution for part 1 of day 3 of the Advent of Code for 2022.
 import sys
 
 
+def process_rucksack(item):
+    priority = 0
+    middle = int(len(item)/2)
+
+    left = item[:middle]
+    left_items = set([x for x in left])
+
+    right = item[middle:]
+    right_items = set([x for x in right])
+
+    for x in left_items:
+        if x in right_items:
+            if x > 'a':
+                priority = ord(x) - ord('a') + 1
+            else:
+                priority = ord(x.lower()) - ord('a') + 27
+            break
+
+    return priority
+
+
 def calculate_solution(items):
     result = 0
+
+    for item in items:
+        result += process_rucksack(item)
 
     return result
 
 
-def run_test(test_input, expected_solution):
+def run_test(test_input, priorities, expected_solution):
     """
     Helper method for running some unit tests whilst minimising repetative code.
     """
+
+    for items, priority in zip(test_input, priorities):
+        result = process_rucksack(items)
+        if result != priority:
+            print(f'Test for {items} FAILED. Got a result of {result}, not {priority}')
+            sys.exit(-1) 
+
     result = calculate_solution(test_input)
 
     if result != expected_solution:
-        print(f'Test for {test_input} FAILED. Got a result of {result}, not {expected_solution}')
+        print(
+            f'Test for {test_input} FAILED. Got a result of {result}, not {expected_solution}')
         sys.exit(-1)
 
     print(f'Test for {test_input} passed.')
@@ -30,9 +62,10 @@ def run_test(test_input, expected_solution):
 # Run any tests that we've defined to help validate our code prior to
 # trying to solve the puzzle.
 
-test_list = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+test_list = ["vJrwpWtwJgWrhcsFMMfFFhFp", "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL", "PmmdzqPrVvPwwTWBwg",
+             "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn", "ttgJtRGJQctTZtZT", "CrZsJsPPZsGzwwsLwLmpwMDw"]
 
-result = run_test(test_list, 7)
+result = run_test(test_list, [16, 38, 42, 22, 20, 19], 157)
 
 print('')
 print('-----------------')
@@ -44,7 +77,7 @@ print('')
 # above is working correctly. Let's use the actual captcha now.
 
 with open('input.txt', 'r') as f:
-    input_data = [int(line) for line in f]
+    input_data = [line for line in f]
     answer = calculate_solution(input_data)
 
     print(f'Solution is {answer}')
