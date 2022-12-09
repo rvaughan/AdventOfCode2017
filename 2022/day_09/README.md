c7ebe91fd9877cede51e352e9422a461e730bed7
Richard Vaughan <richard@purplemonkeys.co.uk>
2022-12-09 05:56:15 +0000

    Add solution for 2022, day 9, part 1.

--- 2022/day_09/README.md
+++ 2022/day_09/README.md
@@ -263,3 +263,5 @@ s###..
 So, there are ```13``` positions the tail visited at least once.
 
 Simulate your complete hypothetical series of motions. **How many positions does the tail of the rope visit at least once?**
+
+**Solution:** 6067
--- 2022/day_09/solution_p1.py
+++ 2022/day_09/solution_p1.py
@@ -6,20 +6,84 @@ This code holds the solution for part 1 of day 9 of the Advent of Code for 2022.
 import sys
 
 
-def calculate_solution(items):
-    result = 0
+def dump_grid(start, tail, head):
+    height = 5
+    width = 6
 
-    return result
+    grid = []
+    for _ in range(height):
+        grid += [['.'] * width]
+
+    grid[start[0]][start[1]] = 's'
+    grid[tail[0]][tail[1]] = 'T'
+    grid[head[0]][head[1]] = 'H'
+
+    for row_idx in range(height):
+        print(f'{"".join([x for x in grid[row_idx]])}')
+    print('------------------------------')
+
+
+def calculate_solution(moves):
+    tail_positions = set()
+
+    start = (4, 0)
+    head = (4, 0)
+    tail = (4, 0)
+
+    tail_positions.add(tail)
+
+    # dump_grid(start, tail, head)
+
+    for move in moves:
+        # print(move)
+
+        direction, cells = move.split(' ')
+
+        for _ in range(int(cells)):
+            if direction == 'L':
+                head = (head[0], head[1] - 1)
+                if (head[1] - tail[1] == -2):
+                    tail = (tail[0], tail[1] - 1)
+                    if head[0] != tail[0]:
+                        tail = (head[0], tail[1])
+                    tail_positions.add(tail)
+            elif direction == 'R':
+                head = (head[0], head[1] + 1)
+                if (head[1] - tail[1] == 2):
+                    tail = (tail[0], tail[1] + 1)
+                    if head[0] != tail[0]:
+                        tail = (head[0], tail[1])
+                    tail_positions.add(tail)
+            elif direction == 'U':
+                head = (head[0] - 1, head[1])
+                if (head[0] - tail[0] == -2):
+                    tail = (tail[0] - 1, tail[1])
+                    if head[1] != tail[1]:
+                        tail = (tail[0], head[1])
+                    tail_positions.add(tail)
+            elif direction == 'D':
+                head = (head[0] + 1, head[1])
+                if (head[0] - tail[0] == 2):
+                    tail = (tail[0] + 1, tail[1])
+                    if head[1] != tail[1]:
+                        tail = (tail[0], head[1])
+                    tail_positions.add(tail)
+
+        # print(head, tail)
+        # dump_grid(start, tail, head)
+
+    return len(tail_positions)
 
 
 def run_test(test_input, expected_solution):
     """
     Helper method for running some unit tests whilst minimising repetative code.
     """
-    result = calculate_solution(test_input)
+    result = calculate_solution(test_input.split('\n'))
 
     if result != expected_solution:
-        print(f'Test for {test_input} FAILED. Got a result of {result}, not {expected_solution}')
+        print(
+            f'Test for {test_input} FAILED. Got a result of {result}, not {expected_solution}')
         sys.exit(-1)
 
     print(f'Test for {test_input} passed.')
@@ -30,9 +94,16 @@ def run_test(test_input, expected_solution):
 # Run any tests that we've defined to help validate our code prior to
 # trying to solve the puzzle.
 
-test_list = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+test_list = """R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2"""
 
-result = run_test(test_list, 7)
+result = run_test(test_list, 13)
 
 print('')
 print('-----------------')
@@ -44,7 +115,7 @@ print('')
 # above is working correctly. Let's use the actual captcha now.
 
 with open('input.txt', 'r') as f:
-    input_data = [int(line) for line in f]
+    input_data = [line.strip() for line in f]
     answer = calculate_solution(input_data)
 
     print(f'Solution is {answer}')
