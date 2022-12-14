0c57913fed9db84b750b33ba78f9cd555a4fe3b6
Richard Vaughan <richard@purplemonkeys.co.uk>
2022-12-14 09:07:42 +0000

    Add solution for 2022, day 14, part 2.

--- 2022/day_14/solution_p2.py
+++ 2022/day_14/solution_p2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-This code holds the solution for part 1 of day 14 of the Advent of Code for 2022.
+This code holds the solution for part 2 of day 14 of the Advent of Code for 2022.
 """
 
 import sys
@@ -18,30 +18,24 @@ def build_grid(lines, rows=1000, cols=1000):
             rock_start = [int(x) for x in rock_positions[idx].split(',')]
             rock_end = [int(x) for x in rock_positions[idx+1].split(',')]
 
-            row_direction = 1
-            if rock_start[0] > rock_end[0]:
-                rock_end[0] -= 1
-                row_direction = -1
-            elif rock_start[0] == rock_end[0]:
-                rock_end[0] += 1
+            rock_start[0], rock_end[0], rock_start[1], rock_end[1] = min(rock_start[0], rock_end[0]), max(rock_start[0], rock_end[0]), min(rock_start[1], rock_end[1]), max(rock_start[1], rock_end[1])
 
-            col_direction = 1
-            if rock_start[1] > rock_end[1]:
-                col_direction = -1
-            elif rock_start[1] == rock_end[1]:
-                rock_end[1] += 1
-
-            for row in range(rock_start[1], rock_end[1], col_direction):
-                for col in range(rock_start[0], rock_end[0], row_direction):
+            for row in range(rock_start[1], rock_end[1]+1):
+                for col in range(rock_start[0], rock_end[0]+1):
                     grid[row][col] = '#'
                     max_depth = max(max_depth, row)
+
+    max_depth = max_depth + 2
+
+    for col in range(cols):
+        grid[max_depth][col] = '#'
 
     return grid, max_depth
 
 
-def dump_grid(grid, start_row=0, end_row=1000, start_col=0, end_col=1000):
+def dump_grid(grid, start_row = 0, end_row = 1000, start_col = 0, end_col = 1000):
     for row in range(start_row, end_row):
-        line = []
+        line=[]
         for col in range(start_col, end_col):
             line.append(grid[row][col])
 
@@ -49,58 +43,55 @@ def dump_grid(grid, start_row=0, end_row=1000, start_col=0, end_col=1000):
 
 
 def calculate_solution(items):
-    grid, max_depth = build_grid(items)
+    grid, max_depth=build_grid(items)
 
-    start_pos = [0, 500]
+    start_pos=[0, 500]
 
     # dump_grid(grid, 0, 11, 490, 510)
     # x
 
-    sand_grains = 0
-    while True:
-        sg_pos = start_pos.copy()
-        moved = True
-        stopped = False
-        while moved and not stopped:
+    sand_grains=0
+    complete = False
+    while not complete:
+        sg_pos=start_pos.copy()
+        stopped=False
+        while not stopped:
             # print(sg_pos)
-
-            if sg_pos[0] > max_depth:
-                print(f'Sand grain {sand_grains+1} went off the board')
-                moved = False
-                break
 
             if grid[sg_pos[0]][sg_pos[1]] != '.':
                 if grid[sg_pos[0]][sg_pos[1]-1] != '.':
                     if grid[sg_pos[0]][sg_pos[1]+1] != '.':
                         # Stopped - so back up one cell
                         sg_pos[0] -= 1
-                        sand_grains += 1
-                        stopped = True
-                        print(f'Sand grain {sand_grains} stopped')
+
+                        if sg_pos[0] == -1:
+                            complete = True
+                        else:
+                            sand_grains += 1
+                        
+                        stopped=True
+                        print(f'Sand grain {sand_grains} stopped [{complete} {sg_pos[0]}]')
                     else:
                         # Move right
                         sg_pos[1] += 1
                 else:
                     # Move left
                     sg_pos[1] -= 1
-
-            if not stopped:
-                grid[sg_pos[0]][sg_pos[1]] = '+'
-                # print('')
-                # dump_grid(grid, 0, 11, 490, 510)
-                grid[sg_pos[0]][sg_pos[1]] = '.'
-
+            else:
                 # Move down one cell
                 sg_pos[0] += 1
-            else:
-                grid[sg_pos[0]][sg_pos[1]] = 'o'
-                # if sand_grains == 24:
-                #     print('')
-                #     dump_grid(grid, 0, 11, 490, 510)
-                #     x
 
-        if moved == False:
-            break
+            if stopped:
+                grid[sg_pos[0]][sg_pos[1]]='o'
+                # if sand_grains == 93:
+                #     print('')
+                #     dump_grid(grid, 0, 12, 489, 512)
+                #     x
+            # else:
+            #     grid[sg_pos[0]][sg_pos[1]]='+'
+            #     print('')
+            #     dump_grid(grid, 0, 11, 490, 510)
+            #     grid[sg_pos[0]][sg_pos[1]]='.'
 
     return sand_grains
 
@@ -109,7 +100,7 @@ def run_test(test_input, expected_solution):
     """
     Helper method for running some unit tests whilst minimising repetative code.
     """
-    result = calculate_solution(test_input.split('\n'))
+    result=calculate_solution(test_input.split('\n'))
 
     if result != expected_solution:
         print(
@@ -124,10 +115,11 @@ def run_test(test_input, expected_solution):
 # Run any tests that we've defined to help validate our code prior to
 # trying to solve the puzzle.
 
-test_list = """498,4 -> 498,6 -> 496,6
+test_list="""498,4 -> 498,6 -> 496,6
 503,4 -> 502,4 -> 502,9 -> 494,9"""
 
-result = run_test(test_list, 24)
+result=run_test(test_list, 93)
+x
 
 print('')
 print('-----------------')
@@ -139,7 +131,7 @@ print('')
 # above is working correctly. Let's use the actual captcha now.
 
 with open('input.txt', 'r') as f:
-    input_data = [line.strip() for line in f]
-    answer = calculate_solution(input_data)
+    input_data=[line.strip() for line in f]
+    answer=calculate_solution(input_data)
 
     print(f'Solution is {answer}')
