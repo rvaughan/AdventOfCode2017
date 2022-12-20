2feb3a49a39a9a22b8255978aef61b4143aaf077
Richard Vaughan <richard@purplemonkeys.co.uk>
2022-12-20 08:09:18 +0000

    Add solution for 2022, day 20, part 1.

--- 2022/day_20/README.md
+++ 2022/day_20/README.md
@@ -65,3 +65,5 @@ Initial arrangement:
 Then, the grove coordinates can be found by looking at the 1000th, 2000th, and 3000th numbers after the value 0, wrapping around the list as necessary. In the above example, the 1000th number after 0 is **4**, the 2000th is **-3**, and the 3000th is **2**; adding these together produces **3**.
 
 Mix your encrypted file exactly once. **What is the sum of the three numbers that form the grove coordinates?**
+
+**Solution:** 1591
--- 2022/day_20/solution_p1.py
+++ 2022/day_20/solution_p1.py
@@ -6,9 +6,29 @@ import sys
 
 
 def calculate_solution(items):
-    result = 0
+    arrangement = [int(x) for x in items]
 
-    return result
+    zero_index = 0
+    mixer = [(idx, a) for (idx, a) in enumerate(arrangement)]
+
+    for i in range(len(arrangement)):
+        for j in range(len(arrangement)):
+            if mixer[j][0] == i:
+                curr = mixer.pop(j)
+                new_index = (j + curr[1]) % len(mixer)
+                mixer.insert(new_index, (i, curr[1]))
+                break
+
+    for (idx, (_, val)) in enumerate(mixer):
+        if val == 0:
+            zero_index = idx
+            break
+
+    return (
+        mixer[(zero_index + 1000) % len(mixer)][1]
+            + mixer[(zero_index + 2000) % len(mixer)][1]
+            + mixer[(zero_index + 3000) % len(mixer)][1]
+    )
 
 
 def run_test(test_input, expected_solution):
@@ -18,7 +38,8 @@ def run_test(test_input, expected_solution):
     result = calculate_solution(test_input.split('\n'))
 
     if result != expected_solution:
-        print(f'Test for {test_input} FAILED. Got a result of {result}, not {expected_solution}')
+        print(
+            f'Test for {test_input} FAILED. Got a result of {result}, not {expected_solution}')
         sys.exit(-1)
 
     print(f'Test for {test_input} passed.')
@@ -29,10 +50,15 @@ def run_test(test_input, expected_solution):
 # Run any tests that we've defined to help validate our code prior to
 # trying to solve the puzzle.
 
-test_list = """
-"""
+test_list = """1
+2
+-3
+3
+-2
+0
+4"""
 
-result = run_test(test_list, 7)
+result = run_test(test_list, 3)
 
 print('')
 print('-----------------')
