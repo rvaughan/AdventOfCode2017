3cc2fbdf4f6b656fd210ba161dd70ae5affa85ef
Richard Vaughan <richard@purplemonkeys.co.uk>
2023-12-02 07:14:53 +0000

    Correct a comment

--- 2023/day_01/solution_p2.py
+++ 2023/day_01/solution_p2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-This code holds the solution for part 1 of day 1 of the Advent of Code for 2023.
+This code holds the solution for part 2 of day 1 of the Advent of Code for 2023.
 """
 import sys
 
