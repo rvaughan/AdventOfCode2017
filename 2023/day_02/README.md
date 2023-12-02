63439458c66c9271f7d85194536f6e81f0bd163a
Richard Vaughan <richard@purplemonkeys.co.uk>
2023-12-02 06:36:52 +0000

    Solution for 2023, day 2, part 1.

--- 2023/day_02/README.md
+++ 2023/day_02/README.md
@@ -14,7 +14,7 @@ As you walk, the Elf shows you a small bag and some cubes which are either red, 
 
 To get information, once a bag has been loaded with cubes, the Elf will reach into the bag, grab a handful of random cubes, show them to you, and then put them back in the bag. He'll do this a few times per game.
 
-You play several games and record the information from each game (your puzzle input). Each game is listed with its ID number (like the 11 in ```Game 11: ...```) followed by a semicolon-separated list of subsets of cubes that were revealed from the bag (like `3 red, 5 green, 4 blue)`.
+You play several games and record the information from each game (your puzzle input). Each game is listed with its ID number (like the `11` in ```Game 11: ...```) followed by a semicolon-separated list of subsets of cubes that were revealed from the bag (like `3 red, 5 green, 4 blue)`.
 
 For example, the record of a few games might look like this:
 
@@ -33,3 +33,5 @@ The Elf would first like to know which games would have been possible if the bag
 In the example above, games 1, 2, and 5 would have been **possible** if the bag had been loaded with that configuration. However, game 3 would have been ***impossible*** because at one point the Elf showed you 20 red cubes at once; similarly, game 4 would also have been **impossible** because the Elf showed you 15 blue cubes at once. If you add up the IDs of the games that would have been possible, you get **8**.
 
 Determine which games would have been possible if the bag had been loaded with only 12 red cubes, 13 green cubes, and 14 blue cubes. **What is the sum of the IDs of those games?**
+
+**Solution:** 2679
--- 2023/day_02/solution_p1.py
+++ 2023/day_02/solution_p1.py
@@ -1,21 +1,50 @@
 #!/usr/bin/env python3
 """
-This code holds the solution for part 1 of day 1 of the Advent of Code for 2023.
+This code holds the solution for part 1 of day 2 of the Advent of Code for 2023.
 """
 import sys
 
 
-def calculate_solution(items):
-    result = 0
+def check_game(game_data, red, green, blue):
+    game_parts = game_data.split('Game ')[1].split(':')
+    
+    game_num = int(game_parts[0])
 
-    return result
+    pulls = game_parts[1].split(';')
+    for pull in pulls:
+        for balls in pull.split(','):
+            count, colour = balls.split()
+            if colour == 'red':
+                if int(count) > red:
+                    return 0
+
+            if colour == 'blue':
+                if int(count) > blue:
+                    return 0
+
+            if colour == 'green':
+                if int(count) > green:
+                    return 0
+
+    return game_num
+
+def calculate_solution(items, red, green, blue):
+    results = 0
+
+    for item in items:
+        if item.strip() == '':
+            continue
+
+        results += check_game(item, red, green, blue)
+
+    return results
 
 
-def run_test(test_input, expected_solution):
+def run_test(test_input, red, green, blue, expected_solution):
     """
     Helper method for running some unit tests whilst minimising repetative code.
     """
-    result = calculate_solution(test_input.split('\n'))
+    result = calculate_solution(test_input.split('\n'), red, green, blue)
 
     if result != expected_solution:
         print(f'Test for {test_input} FAILED. Got a result of {result}, not {expected_solution}')
@@ -30,9 +59,39 @@ def run_test(test_input, expected_solution):
 # trying to solve the puzzle.
 
 test_list = """
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+"""
+result = run_test(test_list, 12, 13, 14, 1)
+
+test_list = """
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+"""
+result = run_test(test_list, 12, 13, 14, 2)
+
+test_list = """
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+"""
+result = run_test(test_list, 12, 13, 14, 0)
+
+test_list = """
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+"""
+result = run_test(test_list, 12, 13, 14, 0)
+
+test_list = """
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+"""
+result = run_test(test_list, 12, 13, 14, 5)
+
+test_list = """
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
 """
 
-result = run_test(test_list, 7)
+result = run_test(test_list, 12, 13, 14, 8)
 
 print('')
 print('-----------------')
@@ -45,6 +104,6 @@ print('')
 
 with open('input.txt', 'r') as f:
     input_data = [line.strip() for line in f]
-    answer = calculate_solution(input_data)
+    answer = calculate_solution(input_data, 12, 13, 14)
 
     print(f'Solution is {answer}')
