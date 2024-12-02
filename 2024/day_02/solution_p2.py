ab0f2f3e45481412bebcf2547134fdd1c3697121
Richard Vaughan <rvaughan@gmail.com>
2024-12-02 06:19:08 +0000

    Set up for 2024, day 2, part 2

---
+++ 2024/day_02/solution_p2.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+"""
+This code holds the solution for part 2 of day 2 of the Advent of Code for 2024.
+"""
+import sys
+
+
+def check_levels(report):
+    ascending = True
+    first = True
+
+    for idx in range(len(report) - 1):
+        if first:
+            if report[idx] > report[idx+1]:
+                ascending = False
+                if abs(report[idx+1] - report[idx]) == 0 or abs(report[idx+1] - report[idx]) > 3:
+                    return False
+                first = False
+        elif ascending and report[idx+1] <= report[idx]:
+            return False
+        elif not ascending and report[idx+1] >= report[idx]:
+            return False
+
+        if abs(report[idx+1] - report[idx]) == 0 or abs(report[idx+1] - report[idx]) > 3:
+            return False
+
+    return True
+
+
+def calculate_solution(reports):
+    result = 0
+
+    for report in reports:
+        report = [int(x) for x in report.split(' ')]
+
+        result += 1 if check_levels(report) else 0
+
+    return result
+
+
+def run_test(test_input, expected_solution):
+    """
+    Helper method for running some unit tests whilst minimising repetative code.
+    """
+    result = calculate_solution(test_input.split('\n'))
+
+    print()
+    if result != expected_solution:
+        print(f'Test for {test_input} FAILED. Got a result of {result}, not {expected_solution}')
+        sys.exit(-1)
+
+    print(f'Test for {test_input} passed.')
+
+    return result
+
+
+# Run any tests that we've defined to help validate our code prior to
+# trying to solve the puzzle.
+
+test_list = """7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9"""
+result = run_test(test_list, 4)
+
+print('')
+print('-----------------')
+print('All Tests PASSED.')
+print('-----------------')
+print('')
+
+# Ok, so if we reach here, then we can be reasonably sure that the code
+# above is working correctly. Let's use the actual captcha now.
+
+with open('input.txt', 'r') as f:
+    input_data = [line.strip() for line in f]
+    answer = calculate_solution(input_data)
+
+    print(f'Solution is {answer}')
