64fb515190cebe76d7919603cef03a54296ff57f
Richard Vaughan <rvaughan@gmail.com>
2024-12-06 07:25:42 +0000

    Solution for 2024, day 6, part 2.

--- 2024/day_06/solution_p2.py
+++ 2024/day_06/solution_p2.py
@@ -5,6 +5,83 @@ This code holds the solution for part 2 of day 6 of the Advent of Code for 2024.
 import sys
 
 
+def dump_grid(grid, height, width, visited):
+    for y in range(height + 1):
+        for x in range(0, width + 1):
+            for direction in range(4):
+                if ((x, y), direction) in visited:
+                    grid[(x,y)] = 'x'
+
+    for y in range(height + 1):
+        row = ''
+        for x in range(0, width + 1):
+            row += grid[(x, y)]
+
+        print(row)
+
+
+def check_obstruction(starting_grid, height, width, start, obstruction):
+    pos = start
+    visited = set()
+    direction = 0
+    visited.add((pos, direction))
+
+    # Can't place the obstacle on the guards starting position.
+    if start == obstruction:
+        return False
+
+    # If there's an obstruction already in that location, just ignore this run as it won't change
+    # anything.
+    if starting_grid[obstruction] == '#':
+        return False
+
+    # Make a copy of the original grid so we don't just end up filling it with obstacles!
+    grid = starting_grid.copy()
+
+    grid[obstruction] = '#'
+
+    spinning = 0
+    while True:
+        if direction == 0: # up
+            next_pos = (pos[0], pos[1] - 1)
+        elif direction == 1: # right
+            next_pos = (pos[0] + 1, pos[1])
+        elif direction == 2: # down
+            next_pos = (pos[0], pos[1] + 1)
+        elif direction == 3: # left
+            next_pos = (pos[0] - 1, pos[1])
+
+        # Have we been here, walking in the same direction previously?
+        if (next_pos, direction) in visited:
+            return True
+
+        # Have we gone off the end of the grid?
+        if next_pos[0] < 0 or next_pos[1] < 0 or next_pos[0] > width or next_pos[1] > height:
+            break
+
+        # Are we blocked in that direction?
+        if grid[next_pos] == '#':
+            direction = (direction + 1) % 4
+
+            # There's a chance we're stuck somewhere, are we just spinning?
+            spinning += 1
+            if spinning > 4:
+                return False
+            
+            continue
+
+        spinning = 0
+
+        # Move there
+        pos = next_pos
+
+        visited.add((pos, direction))
+
+    # dump_grid(grid, width, height, visited)
+
+    return False
+
+
 def calculate_solution(items):
     result = 0
 
@@ -19,33 +96,15 @@ def calculate_solution(items):
     height = len(items) - 1
     width = len(items[0]) - 1
 
-    pos = start
-    visited = set()
-    visited.add(pos)
-    direction = 0
+    # result = check_obstruction(grid, height, width, start, (start[0]-1, start[1]))
+    # result = check_obstruction(grid, height, width, start, (1, 9))
 
-    while True:
-        if direction == 0: # up
-            next_pos = (pos[0], pos[1] - 1)
-        elif direction == 1: # right
-            next_pos = (pos[0] + 1, pos[1])
-        elif direction == 2: # down
-            next_pos = (pos[0], pos[1] + 1)
-        elif direction == 3: # left
-            next_pos = (pos[0] - 1, pos[1])
-
-        if next_pos[0] < 0 or next_pos[1] < 0 or next_pos[0] > width or next_pos[1] > height:
-            break
-
-        if grid[next_pos] == '#':
-            direction = (direction + 1) % 4
-            continue
-
-        pos = next_pos
-
-        visited.add(pos)
-
-    result = len(visited)
+    for x in range(width + 1):
+        for y in range(height + 1):
+            obstruction = (x, y)
+            if check_obstruction(grid, height, width, start, obstruction):
+                result += 1
+                # print(f'Found solution at {obstruction}')
 
     return result
 
@@ -79,7 +138,7 @@ test_list = """....#.....
 ........#.
 #.........
 ......#..."""
-result = run_test(test_list, 41)
+result = run_test(test_list, 6)
 
 print('')
 print('-----------------')
