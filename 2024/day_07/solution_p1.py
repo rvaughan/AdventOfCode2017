367f1e04f7267c3df7ed89c688c26fd27482b4d1
Richard Vaughan <rvaughan@gmail.com>
2024-12-07 07:28:25 +0000

    Info for 2024, day 7, part 1

--- 2024/day_07/solution_p1.py
+++ 2024/day_07/solution_p1.py
@@ -30,9 +30,16 @@ def run_test(test_input, expected_solution):
 # Run any tests that we've defined to help validate our code prior to
 # trying to solve the puzzle.
 
-test_list = """
-"""
-result = run_test(test_list, 7)
+test_list = """190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20"""
+result = run_test(test_list, 3749)
 
 print('')
 print('-----------------')
